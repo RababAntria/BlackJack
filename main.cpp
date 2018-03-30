a0d3bbba5446048a54fb4ce7bd778d6232c08c23
RababAntria <rabab.antria@mavs.uta.edu>
2018-03-30 17:05:16 -0500

    main addded

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "Dealer.h"
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Group.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/fl_ask.H>
+#include <FL/Fl_Button.H>
+
+
+const int WIDTH = 800;
+const int HEIGHT = 450;
+
+
+using namespace std;
+
+Fl_Wndow *win, *windo;
+Fl_Group *Players;
+ 
